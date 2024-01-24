31a777406bd6f2d2ebd6b2c9c9770e62238de3ea
Smj0hnson614 <153125119+Smj0hnson614@users.noreply.github.com>
2024-01-24 06:37:58 -0500

    Addede basic JavaScript and typewriter plugin

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: "Quote is here",
+    autoStart: true,
+    delay: 1,
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
