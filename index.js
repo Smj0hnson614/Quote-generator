c6fcfe067c4f2444d1fd241058b37dacd026bd59
Smj0hnson614 <153125119+Smj0hnson614@users.noreply.github.com>
2024-01-24 16:17:18 -0500

    Improved the User Experience (UX)

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function generateQuote(event) {
   let prompt = `User instructions: Generate a short quote about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating quote");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let quoteElement = document.querySelector("#quote");
+  quoteElement.classList.remove("hidden");
+  quoteElement.innerHTML = `<div class="blink">⏳ Generating an inspirational quote about ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayQuote);
 }
